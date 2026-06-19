96c2a169e2e001e9d24739f42369952b12daa8f7
Coolbit <neeoub@gmail.com>
2026-06-19 17:13:55 +0545

    Feat: Add, Remove, Mark, List features done

--- todo.go
+++ todo.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/google/uuid"
 )
@@ -34,6 +35,34 @@ func load_all_todo() {
 	json.Unmarshal(content, &all_todos)
 }
 
+func write_todo_json() {
+	data, _ := json.Marshal(all_todos)
+	os.WriteFile("todo.json", data, 0644)
+}
+
+func parse_sts(status string) Status {
+	var sts Status
+	switch strings.ToLower(status) {
+	case "completed":
+		sts = StatusCompleted
+	case "wip":
+		sts = StatusInProgress
+	case "pending":
+		sts = StatusPending
+	default:
+		sts = StatusPending
+	}
+	return sts
+}
+
+func check_task_id_length(task_id string) {
+	if len(task_id) < 4 {
+		fmt.Println("The ID prefix should be greater than 4 characters.")
+		os.Exit(1)
+		return
+	}
+}
+
 func main() {
 	load_all_todo()
 	command := os.Args[1]
@@ -45,7 +74,15 @@ func main() {
 		list_task()
 	case "mark":
 		task_id := os.Args[2]
-		fmt.Println(task_id)
+		new_status := os.Args[3]
+		check_task_id_length(task_id)
+		mark_task(new_status, task_id)
+
+	case "del":
+	case "delete":
+		task_id := os.Args[2]
+		check_task_id_length(task_id)
+		remove_task(task_id)
 	default:
 		fmt.Println("Unknown command")
 	}
@@ -56,8 +93,7 @@ func add_task(task string) {
 	id := uuid.New()
 	var t = todo{Id: id, Task: task, TaskStatus: StatusPending}
 	all_todos = append(all_todos, t)
-	data, _ := json.Marshal(all_todos)
-	os.WriteFile("todo.json", data, 0644)
+	write_todo_json()
 }
 
 func list_task() {
@@ -65,4 +101,28 @@ func list_task() {
 	for i := range all_todos {
 		fmt.Println("Task ID: ", all_todos[i].Id, "\nTask: ", all_todos[i].Task, "\nTask Status: ", all_todos[i].TaskStatus)
 	}
+}
+
+func mark_task(status string, taskID string) {
+	load_all_todo()
+	var sts Status
+	sts = parse_sts(status)
+	for i := range all_todos {
+		if strings.HasPrefix(all_todos[i].Id.String(), taskID) {
+			all_todos[i].TaskStatus = sts
+			fmt.Println(all_todos[i])
+		}
+	}
+	write_todo_json()
+}
+
+func remove_task(taskID string) {
+	load_all_todo()
+	for i := 0; i < len(all_todos); i++ {
+		if strings.HasPrefix(all_todos[i].Id.String(), taskID) {
+			all_todos = append(all_todos[:i], all_todos[i+1:]...)
+			fmt.Println("Task Deleted")
+		}
+	}
+	write_todo_json()
 }
